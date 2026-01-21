9e829f62f6fc465263fe7e4cf8ca3ed9198a8b46
Incognito <lada.kashubo@yandex.ru>
2026-01-21 22:51:06 +0300

    Introduction to Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,96 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+const val bConst:Int=10
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    println("123")
+    //var a = 10
+    //var a:Int = 10
+    //a = 3
+    //val a = 10
+    //val - не можем изменить
+    //var - можем менять
+    //val a: Long = 22L
+    //val a: UInt = 22U
+    //val a:Float = 1.78F
+    //val a: Boolean = true
+    //val a:String = "23"
+    /*val name = "Anna"
+    val lastName = "Petrova"
+    val fullName = "Hello $name $lastName"
+    println(fullName)*/
+    /*var longN:Long = 2
+    var intN: Int = 4
+    longN = intN.toLong()
+    println(longN)*/
+    /*var name:Any = "Tom"
+    name = 979879*/
+    //val name = readLine()
+    /*val x = 11
+    var y = 5
+    val z = x/y
+    println(z)
+    val q = y++*/
+    /*val a = 11
+    val b = 5
+    val c: Boolean = a>b
+    println(c)*/
+    /*val a = true
+    val b = false
+    val c = a and b//&&
+    val d = a or b//||
+    val e = a xor b//!=
+    println(c)*/
+    /*val a = 5
+    val b = a in 1..6
+    println(b)
+    if (a==0){
+        println("a=$a")
     }
+    val c = if(a>4){
+        println(a)
+        a
+    }else{
+        println("a")
+        a+1
+    }*/
+
+    /*val isEnable = true
+    when(isEnable){
+        false->println("qwerty")
+        true->println("12345")
+    }*/
+
+    /*var a = 30
+    when(a){//аналог switch
+        10, 15-> {
+            println("qwerty")
+            a*=2
+        }
+        in 20..30->println("12345")
+        else->println("1234567")
+    }*/
+
+    /*for(n in 1..9){
+        println("${n*n}\t")
+    }*/
+
+    /*val range = 1..5
+    val range1 = 10 downTo 1 step 3
+    val range2 = 1 until 5
+    print(range1)*/
+
+    /*val num: Array<Int> = arrayOf(1, 2, 3)
+    val arr = arrayOf(1,2,3)
+    val nom = arrayOfNulls<Int>(3)*/
+    /*var num = IntArray(3)
+    num[0] = 10
+    num[1] = 20
+    num = IntArray(5)*/
+
+    /*val num = Array(3, {5})
+    for(number in num){
+        print("$number ")
+    }*/
+    /*val num = (1..5).toList().toIntArray()
+    for(i in 0 until num.size step 2){
+        println(num[i])
+    }*/
 }
