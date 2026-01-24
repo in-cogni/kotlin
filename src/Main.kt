2788fb40faa1e0467016c7ade0b07d9face4b3c3
Incognito <lada.kashubo@yandex.ru>
2026-01-24 13:15:00 +0300

    Args, functions, vararg

--- src/Main.kt
+++ src/Main.kt
@@ -1,36 +1,55 @@
 fun main() {
     //1
-    val arr = arrayOf(1,2,3,4,5,6,7,8,9,10)
-    for(num in 1..arr.size step 2){
-        println(arr[num])
-    }
+    val arr: Array<Int> = arrayOf(1, 2, 3, 4, 2)
+    val s = sum(arr)
+    println("№1: sum=${s}")
 
     //2
-    val arr = arrayOf(-3, 1,2, 3, 4,0, 60, 18, 35)
-    for(i in arr){
-        when{
-            i<0 -> println("$i negative")
-            i==0 -> println("$i zero")
-            i in 1..9 -> println("$i small")
-            i>10 -> println("$i big")
-        }
-    }
+    var m = max(1, 2, 3, 1, 9, 0, 2)
+    println("№2: max=${m}")
+    m  = max(2, 6)
+    println("\tmax=${m}")
 
     //3
-    print("Enter number: ")
-    var n = readLine()!!.toInt()
-    val arr = (-n..n).toList().toIntArray()
-    var a = 0
-    for(i in 1 until arr.size step 2){
-        a=arr[i]
-        when{
-            a<0 -> println("$a minus")
-            a==0 -> println("$a zero")
-            a>0 && a%2==0 -> println("$a even")
-            a>0 && a%2!=0 -> println("$a odd")
-            else ->println(a)
+    val arr1: IntArray = intArrayOf(1, 4, 56, 23, 98)
+    val arr2: IntArray = filterEven(arr1)
+    print("№3: ")
+    printNumbers(*arr2)
+}
+fun sum(numbers: Array<Int>): Int{
+    var s = 0
+    for(i in numbers){
+        s+=i
+    }
+    return s
+}
+
+fun max(vararg num: Int):Int{
+    var maxValue = num[0]
+    for(i in num){
+        if(i > maxValue){
+            maxValue = i
         }
     }
+    return maxValue
+}
 
+fun filterEven(arr: IntArray): IntArray{
+    val newArr = IntArray(arr.size)
 
+    var i = 0
+    for(n in arr){
+        if(n%2==0){
+            newArr[i] = n
+            i++
+        }
+    }
+    return newArr.copyOfRange(0, i)
+}
+
+fun printNumbers(vararg num:Int){
+    for(n in num){
+        print("${n} ")
+    }
+    println()
 }
